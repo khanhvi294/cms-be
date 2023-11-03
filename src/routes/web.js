84d7d88fa0e4e4cdebe9dd6734c2554f9739e450
Vi Nguyen <khanhvi2941@gmail.com>
2023-11-03 12:01:03 +0700

    token

--- src/routes/web.js
+++ src/routes/web.js
@@ -2,13 +2,15 @@ import express from "express";
 
 const router = express.Router();
 import homeController from "../controllers/homeController.js";
+import loginController from "../controllers/loginController.js";
 
 /**
  * @param {*} app : express app
  */
 const initWebRoutes = (app) => {
   router.get("/", homeController.handleHi);
-  return app.use("/", router);
+  router.post("/login", loginController.handleLogin);
+  return app.use("/api", router);
 };
 
 export default initWebRoutes;
