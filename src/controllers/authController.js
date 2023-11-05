1a76e97283ebbc9459efcdc564ae94280f762aa0
Vi Nguyen <khanhvi2941@gmail.com>
2023-11-05 15:57:37 +0700

    up

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,8 +1,13 @@
 import authService from "../services/authService";
+import { STATUS_CODE } from "./base";
 
 const login = async (req, res) => {
-  await authService.login();
-  return res.status(200).json({ message: "loginapi" });
+  try {
+    const result = await courseService.login();
+    successResponse(STATUS_CODE.OK, result, res);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export default {
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,10 +1,16 @@
+import db from "../models";
+
 const login = async (req, res) => {
-  const user = await Account.findOne({
-    where: { email: req.email, password: req.password },
-  });
   const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
   res.header("auth-token", token).send(token);
-  return res.send(token);
+
+  const user = await db.Account.findOne({
+    where: { email: req.email, password: req.password },
+  });
+  return {
+    data: user,
+    total: user.length,
+  };
 };
 
 export default { login };
