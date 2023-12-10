840c21b9f96b6d7822a7472d1377d1c6f38caae9
minhnhat165 <nguyenminhnhat301101@gmail.com>
2023-12-10 17:06:23 +0700

    Add forgot password

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,46 +1,87 @@
-import authService from "../services/authService";
 import {
-  successResponse,
-  STATUS_CODE,
-  errorValidateResponse,
-} from "./baseController";
-import userValidate from "../validations/userValidation";
-import { validateData } from "../utils/validateData";
+	STATUS_CODE,
+	errorValidateResponse,
+	successResponse,
+} from './baseController';
+
+import authService from '../services/authService';
+import authValidate from '../validations/authValidation';
+import userValidate from '../validations/userValidation';
+import { validateData } from '../utils/validateData';
 
 const login = async (req, res, next) => {
-  try {
-    const err = await validateData(userValidate.login, req.body);
-    if (err) {
-      return errorValidateResponse(422, err, res);
-    }
-    const result = await authService.login(req.body.email, req.body.password);
+	try {
+		const err = await validateData(userValidate.login, req.body);
+		if (err) {
+			return errorValidateResponse(422, err, res);
+		}
+		const result = await authService.login(
+			req.body.email,
+			req.body.password,
+		);
 
-    successResponse(STATUS_CODE.OK, result, res);
-  } catch (error) {
-    next(error);
-  }
+		successResponse(STATUS_CODE.OK, result, res);
+	} catch (error) {
+		next(error);
+	}
 };
 
 const getInfo = async (req, res, next) => {
-  try {
-    const user = await authService.getInfo(req.user.id);
-    successResponse(STATUS_CODE.OK, user, res);
-  } catch (error) {
-    next(error);
-  }
+	try {
+		const user = await authService.getInfo(req.user.id);
+		successResponse(STATUS_CODE.OK, user, res);
+	} catch (error) {
+		next(error);
+	}
 };
 
 const changePassword = async (req, res, next) => {
-  try {
-    const user = await authService.changePassword(req.body, req.user.id);
-    successResponse(STATUS_CODE.OK, user, res);
-  } catch (error) {
-    next(error);
-  }
+	try {
+		const user = await authService.changePassword(req.body, req.user.id);
+		successResponse(STATUS_CODE.OK, user, res);
+	} catch (error) {
+		next(error);
+	}
+};
+
+const forgotPassword = async (req, res, next) => {
+	try {
+		const err = await validateData(authValidate.forgotPassword, req.body);
+		if (err) {
+			return errorValidateResponse(422, err, res);
+		}
+		await authService.forgotPassword(req.body.email);
+		successResponse(
+			STATUS_CODE.OK,
+			{
+				message: 'Please check your email to reset password',
+			},
+			res,
+		);
+	} catch (error) {
+		next(error);
+	}
+};
+
+const resetPassword = async (req, res, next) => {
+	try {
+		await authService.resetPassword(req.body);
+		successResponse(
+			STATUS_CODE.OK,
+			{
+				message: 'Reset password successfully',
+			},
+			res,
+		);
+	} catch (error) {
+		next(error);
+	}
 };
 
 export default {
-  login,
-  getInfo,
-  changePassword,
+	login,
+	getInfo,
+	changePassword,
+	forgotPassword,
+	resetPassword,
 };
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,122 +1,135 @@
-import ErrorMessage from "../common/errorMessage";
-import HttpException from "../errors/httpException";
-import db from "../models";
-import { ROLES } from "../utils/const";
-import jwtUtil from "../utils/jwt";
-import passwordUtil from "../utils/password";
-import { Op } from "sequelize";
+import ErrorMessage from '../common/errorMessage';
+import HttpException from '../errors/httpException';
+import { Op } from 'sequelize';
+import { ROLES } from '../utils/const';
+import db from '../models';
+import jwtUtil from '../utils/jwt';
+import { mailService } from './mailService';
+import passwordUtil from '../utils/password';
+
+// lazy for update db column for reset code, so we use this
+const resetCodes = {};
+// ex: resetCodes[email] = {code: '123456', expiredAt: 1234567890}
+// ts ver
+// type ResetCode = {
+//   code: string,
+//   expiredAt: number,
+// }
+// type ResetCodes = {
+//   [key: string]: ResetCode,
+// }
 
 export const authAccount = async (email, password) => {
-  let account = await db.Account.findOne({ where: { email } });
-  if (!account) {
-    throw new HttpException(404, ErrorMessage.LOGIN_FAILED);
-  }
-  const isPasswordMatch = await passwordUtil.comparePassword(
-    account.password,
-    password
-  );
-  if (!isPasswordMatch) {
-    throw new HttpException(404, ErrorMessage.LOGIN_FAILED);
-  }
-  if (!account.isActive) {
-    throw new HttpException(
-      422,
-      "Tài khoản của bạn đang bị khóa, liên hệ quản trị viên để được hỗ trợ !!!"
-    );
-  }
-  return account;
+	let account = await db.Account.findOne({ where: { email } });
+	if (!account) {
+		throw new HttpException(404, ErrorMessage.LOGIN_FAILED);
+	}
+	const isPasswordMatch = await passwordUtil.comparePassword(
+		account.password,
+		password,
+	);
+	if (!isPasswordMatch) {
+		throw new HttpException(404, ErrorMessage.LOGIN_FAILED);
+	}
+	if (!account.isActive) {
+		throw new HttpException(
+			422,
+			'Tài khoản của bạn đang bị khóa, liên hệ quản trị viên để được hỗ trợ !!!',
+		);
+	}
+	return account;
 };
 
 export const checkEmailIsExistsExceptId = async (email, id) => {
-  if (!email || !id) {
-    throw new HttpException(404, ErrorMessage.MISSING_PARAMETER);
-  }
+	if (!email || !id) {
+		throw new HttpException(404, ErrorMessage.MISSING_PARAMETER);
+	}
 
-  const account = await db.Account.findOne({
-    where: { email: email, id: { [Op.ne]: id } },
-  });
+	const account = await db.Account.findOne({
+		where: { email: email, id: { [Op.ne]: id } },
+	});
 
-  if (account) {
-    throw new HttpException(422, ErrorMessage.EMAIL_IS_EXISTING);
-  }
-  return false;
+	if (account) {
+		throw new HttpException(422, ErrorMessage.EMAIL_IS_EXISTING);
+	}
+	return false;
 };
 
 export const findAccount = async (id) => {
-  let account = await db.Account.findOne({ where: { id } });
-  return account;
+	let account = await db.Account.findOne({ where: { id } });
+	return account;
 };
 
 export const getStudentByAccount = async (id) => {
-  const student = await db.Account.findOne({
-    where: { id },
-    raw: true,
-    nest: true,
-    attributes: { exclude: ["accountId", "password"] },
-    include: [
-      {
-        model: db.Students,
-        as: "accountStudent",
-        attributes: [
-          "id",
-          "fullName",
-          "gender",
-          "dateOfBirth",
-          "address",
-          "phone",
-        ],
-      },
-    ],
-  });
-  return student;
+	const student = await db.Account.findOne({
+		where: { id },
+		raw: true,
+		nest: true,
+		attributes: { exclude: ['accountId', 'password'] },
+		include: [
+			{
+				model: db.Students,
+				as: 'accountStudent',
+				attributes: [
+					'id',
+					'fullName',
+					'gender',
+					'dateOfBirth',
+					'address',
+					'phone',
+				],
+			},
+		],
+	});
+	return student;
 };
 
 export const getEmployeeByAccount = async (id) => {
-  const employee = await db.Account.findOne({
-    where: { id },
-    raw: true,
-    nest: true,
-    attributes: { exclude: ["accountId", "password"] },
-    attributes: ["id", "email", "role"],
-    include: [
-      {
-        model: db.Employee,
-        as: "accountEmployee",
-        attributes: [
-          "id",
-          "fullName",
-          "cccd",
-          "gender",
-          "dateOfBirth",
-          "address",
-          "phone",
-        ],
-      },
-    ],
-  });
+	const employee = await db.Account.findOne({
+		where: { id },
+		raw: true,
+		nest: true,
+		attributes: { exclude: ['accountId', 'password'] },
+		attributes: ['id', 'email', 'role'],
+		include: [
+			{
+				model: db.Employee,
+				as: 'accountEmployee',
+				attributes: [
+					'id',
+					'fullName',
+					'cccd',
+					'gender',
+					'dateOfBirth',
+					'address',
+					'phone',
+				],
+			},
+		],
+	});
 
-  return employee;
+	return employee;
 };
 
 export const getInfo = async (id) => {
-  const account = await findAccount(id);
-  if (!account) {
-    throw new HttpException(422, "Account is not exits");
-  }
-  if (account.role == ROLES.STUDENT) {
-    return getStudentByAccount(id);
-  }
-  return getEmployeeByAccount(id);
+	const account = await findAccount(id);
+	if (!account) {
+		throw new HttpException(422, 'Account is not exits');
+	}
+	if (account.role == ROLES.STUDENT) {
+		return getStudentByAccount(id);
+	}
+	return getEmployeeByAccount(id);
 };
 
 export const findAccountByEmail = async (email) => {
-  let account = await db.Account.findOne({ where: { email } });
-  return account;
+	let account = await db.Account.findOne({ where: { email } });
+	return account;
 };
 
 export const findAccountById = async (id) => {
-  let account = await db.Account.findOne({ where: { id } });
-  return account;
+	let account = await db.Account.findOne({ where: { id } });
+	return account;
 };
 
 // export const checkEmailAccountUpdate = async (account) => {
@@ -127,62 +140,108 @@ export const findAccountById = async (id) => {
 // };
 
 export const getAccountByEmail = async (email) => {
-  const account = await findAccountByEmail(email);
-  if (!account) {
-    throw new HttpException(404, "Account not found");
-  }
+	const account = await findAccountByEmail(email);
+	if (!account) {
+		throw new HttpException(404, 'Account not found');
+	}
 };
 
 const login = async (email, password) => {
-  const account = await authAccount(email, password);
+	const account = await authAccount(email, password);
 
-  const token = await jwtUtil.generateToken({ id: account.id });
-  return { token };
+	const token = await jwtUtil.generateToken({ id: account.id });
+	return { token };
 };
 
 const deleteAccount = async (id) => {
-  const haveAccount = await findAccount(id);
-  if (!haveAccount) {
-    throw new HttpException(
-      400,
-      ErrorMessage.OBJECT_IS_NOT_EXISTING("Account")
-    );
-  }
-  const account = await db.Account.destroy({
-    where: {
-      id: id,
-    },
-  });
+	const haveAccount = await findAccount(id);
+	if (!haveAccount) {
+		throw new HttpException(
+			400,
+			ErrorMessage.OBJECT_IS_NOT_EXISTING('Account'),
+		);
+	}
+	const account = await db.Account.destroy({
+		where: {
+			id: id,
+		},
+	});
 
-  return account;
+	return account;
 };
 
 export const changePassword = async (data, accountId) => {
-  const account = await findAccountById(accountId);
-  const password = await passwordUtil.generateHashPassword(data.password);
-  const isPasswordMatch = await passwordUtil.comparePassword(
-    account.password,
-    password
-  );
-  if (!isPasswordMatch) {
-    throw new HttpException(404, ErrorMessage.PASSWORD_IS_INCORRECT);
-  }
-  const hashPassword = await passwordUtil.generateHashPassword(data.newPassword);
+	const account = await findAccountById(accountId);
+	const password = await passwordUtil.generateHashPassword(data.password);
+	const isPasswordMatch = await passwordUtil.comparePassword(
+		account.password,
+		password,
+	);
+	if (!isPasswordMatch) {
+		throw new HttpException(404, ErrorMessage.PASSWORD_IS_INCORRECT);
+	}
+	const hashPassword = await passwordUtil.generateHashPassword(
+		data.newPassword,
+	);
 
-  const upPass = await db.Account.update(hashPassword, {
-    where: { id: accountId },
-  });
-  return upPass;
+	const upPass = await db.Account.update(hashPassword, {
+		where: { id: accountId },
+	});
+	return upPass;
+};
+
+export const forgotPassword = async (email) => {
+	const account = await findAccountByEmail(email);
+	if (!account) {
+		throw new HttpException(404, ErrorMessage.EMAIL_IS_NOT_EXISTING);
+	}
+
+	// remove old reset code
+	if (resetCodes[email]) {
+		delete resetCodes[email];
+	}
+	const code = Math.floor(100000 + Math.random() * 900000); // random 6 number
+	const expiredAt = Date.now() + 5 * 60 * 1000; // expired after 5 mins
+	resetCodes[email] = { code, expiredAt };
+	// send email
+	const mailData = {
+		to: email,
+		subject: 'Reset password code',
+		text: `Your reset password code is ${code}`,
+	};
+	await mailService.sendMail(mailData);
+	return true;
+};
+export const resetPassword = async (data) => {
+	const { code, password, email } = data;
+	if (!resetCodes[email]) {
+		throw new HttpException(404, 'Invalid or expired reset code');
+	}
+	if (resetCodes[email].code !== parseInt(code)) {
+		throw new HttpException(400, 'Reset code is incorrect');
+	}
+	if (Date.now() > resetCodes[email].expiredAt) {
+		throw new HttpException(400, 'Reset code is expired');
+	}
+	const hashPassword = await passwordUtil.generateHashPassword(password);
+	const account = await findAccountByEmail(email);
+	await db.Account.update(
+		{ password: hashPassword },
+		{ where: { id: account.id } },
+	);
+	return true;
 };
 
 export default {
-  login,
-  findAccount,
-  findAccountByEmail,
-  getInfo,
-  getEmployeeByAccount,
-  getStudentByAccount,
-  deleteAccount,
-  checkEmailIsExistsExceptId,
-  changePassword,
+	login,
+	findAccount,
+	findAccountByEmail,
+	getInfo,
+	getEmployeeByAccount,
+	getStudentByAccount,
+	deleteAccount,
+	checkEmailIsExistsExceptId,
+	changePassword,
+	forgotPassword,
+	resetPassword,
 };
